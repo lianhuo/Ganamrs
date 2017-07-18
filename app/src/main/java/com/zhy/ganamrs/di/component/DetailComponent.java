87acd440f71add347c6bb56922e19f4b2a45d12e
lianhuo <1848464395@qq.com>
2017-07-18 09:03:59 +0800

    ubuntu

--- app/src/main/java/com/zhy/ganamrs/di/component/DetailComponent.java
+++ app/src/main/java/com/zhy/ganamrs/di/component/DetailComponent.java
@@ -1,14 +1,11 @@
 package com.zhy.ganamrs.di.component;
 
+import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.di.scope.ActivityScope;
+import com.zhy.ganamrs.di.module.DetailModule;
+import com.zhy.ganamrs.mvp.ui.activity.DetailActivity;
 
 import dagger.Component;
-
-import com.jess.arms.di.component.AppComponent;
-
-import com.zhy.ganamrs.di.module.DetailModule;
-
-import com.zhy.ganamrs.mvp.ui.activity.DetailActivity;
 
 @ActivityScope
 @Component(modules = DetailModule.class, dependencies = AppComponent.class)
